4c7968ca8275afdb843d3eba76256b9c5ca69379
klmin <haemmas6394@gmail.com>
2025-02-04 14:22:40 +0900

    fix: Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-import React from "react";
-import Counter from "./components/Counter";
-import UserList from "./components/UserList";
-import WindowSize from "./components/WindowSize";
-=======
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
->>>>>>> feat/router
 
 const App = () => {
   return (
@@ -18,14 +8,4 @@ const App = () => {
     </BrowserRouter>
   );
 };
-<<<<<<< HEAD
->>>>>>> feat/custom-hook
-
-import ParentComponent from './ParentComponent';
-import ParentComponentOptimized from './ParentComponentOptimized'
-
-export default ParentComponentOptimized;
-=======
->>>>>>> feat/router
-
 export default App;
